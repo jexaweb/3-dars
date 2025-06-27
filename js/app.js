b470049c5f02eaf96ff07191ee5bb5af5961462a
kamoliddin <kamoliddin@gmail.com>
2025-06-27 15:52:03 +0500

    a

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const playBt = document.querySelector("#play");
-const backwardBt = document.querySelector("#backwardbt");
+const backwardBt = document.querySelector("#backward");
 const forwardBt = document.querySelector("#forward");
 const audio = document.querySelector("audio");
 
@@ -8,6 +8,43 @@ const progresscontainer = document.querySelector(".progress-container");
 const progressEl = document.querySelector(".progress");
 const volumeChanger = document.querySelector("#volume-Changer");
 audio.volume = +volumeChanger.value / 100;
+const cover = document.getElementById("cover");
+const musictitle = document.getElementById("music-title");
+const durationEl = document.getElementById("duration");
+const currenttimeEl = document.getElementById("current - time");
+
+audio.addEventListener("loadeddata", () => {
+  const duration = audio.duration;
+  const minutes = String((duration - (duration % 60)) / 60);
+  const seconds = String(parseInt(duration % 60));
+  let time = `${+minutes < 10 ? `${minutes.padStart(2, 0)}` : minutes}
+  :${+minutes < 10 ? `${seconds.padStart(2, 0)}` : seconds}`;
+  durationEl.textContent = time;
+  // currenttimeEl.textContent = time;
+});
+
+audio.addEventListener("loadedmetadata", () => {
+  const duration = audio.duration;
+  const minutes = String(Math.floor(duration / 60)).padStart(2, "0");
+  const seconds = String(Math.floor(duration % 60)).padStart(2, "0");
+  durationEl.textContent = `${minutes}:${seconds}`;
+});
+
+const songs = [
+  "J.Y. Park - When We Disco",
+  "Kim BumSoo - I Miss You ",
+  "어쩌다 마주친 그대 - 미도와 파라솔 ",
+];
+let currentPlayingSong = 1;
+
+function changeSong(current) {
+  audio.src = `../audios/${songs[current]}.mp3`;
+  cover.src = `../imgs/${songs[current]}.jpg`;
+  musictitle.textContent = songs[current];
+}
+
+changeSong(currentPlayingSong);
+
 function pause() {
   audio.pause();
   container.classList.remove("play");
@@ -18,6 +55,26 @@ function play() {
   audio.play();
   container.classList.add("play");
   playBt.innerHTML = '<i class="fa-solid fa-pause"></i>';
+}
+
+function nextSong() {
+  if (currentPlayingSong < songs.length - 1) {
+    currentPlayingSong++;
+  } else {
+    currentPlayingSong = 0;
+  }
+  changeSong(currentPlayingSong);
+  play();
+}
+
+function preevSong() {
+  if (currentPlayingSong > 0) {
+    currentPlayingSong--;
+  } else {
+    currentPlayingSong = songs.length - 1;
+  }
+  changeSong(currentPlayingSong);
+  play();
 }
 
 function muscplay() {
@@ -33,17 +90,40 @@ function progress() {
   const duration = audio.duration;
   const currentTime = audio.currentTime;
 
+  const decresingTime = isNaN(duration + currentTime)
+    ? 0
+    : duration - currentTime;
+
+  const minutes = String((decresingTime - (decresingTime % 60)) / 60);
+  const seconds = String(parseInt(decresingTime % 60));
+  let time = `${+minutes < 10 ? `${minutes.padStart(2, 0)}` : minutes}
+  :${+minutes < 10 ? `${seconds.padStart(2, 0)}` : seconds}`;
+  currenttimeEl.textContent = time;
+
   const p = (currentTime / duration) * 100;
   progressEl.style.width = `${p}%`;
 }
+
 function changeTime(e) {
   const p = (e.offsetX / this.clientWidth) * 100;
   const currentTime = (audio.duration / 100) * p;
   audio.currentTime = currentTime;
 }
+
+function volumechanger() {
+  audio.volume = +volumeChanger.value / 100;
+  volumeEl.textContent = +volumeChanger / value;
+}
+
+// volumeChanger.addEventListener("input", () => {
+//   audio.volume = +volumeChanger.value / 100;
+// });
+
 playBt.addEventListener("click", muscplay);
 audio.addEventListener("timeupdate", progress);
 progresscontainer.addEventListener("click", changeTime);
-volumeChanger.addEventListener("input", () => {
-  audio.volume = +volumeChanger.value / 100;
-});
+// volumeChanger.addEventListener("input", volumeChanger);
+volumeChanger.addEventListener("input", volumechanger);
+audio.addEventListener("ended", nextSong);
+backwardBt.addEventListener("click", preevSong);
+forwardBt.addEventListener("click", nextSong);
